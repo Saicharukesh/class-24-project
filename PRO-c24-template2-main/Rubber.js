b9d726d073777877791328dc8907aa1bb3838fe8
shirishagajam <64522203+shirishagajam@users.noreply.github.com>
2021-06-10 13:24:23 +0530

    Update Rubber.js

--- PRO-c24-template2-main/Rubber.js
+++ PRO-c24-template2-main/Rubber.js
@@ -19,12 +19,12 @@ class Rubber {
 			var rubberpos=this.body.position;		
 			push()
 			translate(rubberpos.x, rubberpos.y);
-			rectMode(CENTER)
+			ellipseMode(RADIUS)
 			strokeWeight(4);
 			stroke("black");
 			fill("darkblue");
 			//draw the ellipse here to display the rubber ball
-
+			ellipse(0,0,this.r, this.r);
 			pop()
 	}
 
